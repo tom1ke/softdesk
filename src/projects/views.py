a1836017e06dbe2a0b233f6fb70ac387c9f79a96
Thomas Roussel <thomas.roussel@ik.me>
2022-06-30 18:35:53 +0200

    projects app viewsets

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Project, Contributor, Issue, Comment
+from .serializers import (
+    ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer, IssueListSerializer, IssueDetailSerializer,
+    CommentListSerializer, CommentDetailSerializer
+)
+
+
+class MultipleSerializerMixin:
+
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return super().get_serializer_class()
+
+
+class ProjectViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = ProjectListSerializer
+    detail_serializer_class = ProjectDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.all()
+
+
+class ContributorViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Contributor.objects.all()
+
+
+class IssueViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = IssueListSerializer
+    detail_serializer_class = IssueDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Issue.objects.all()
+        project_id = self.request.GET.get('project_id')
+        if project_id is not None:
+            queryset = queryset.filter(project_id=project_id)
+        return queryset
+
+
+class CommentViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+
+    serializer_class = CommentListSerializer
+    detail_serializer_class = CommentDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Comment.objects.all()
+        issue_id = self.request.GET.get('issue_id')
+        if issue_id is not None:
+            queryset = queryset.filter(issue_id=issue_id)
+        return queryset
