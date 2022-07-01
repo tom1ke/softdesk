0e02929deda096ab6d740665505c2e23b3f8fc3b
Thomas Roussel <thomas.roussel@ik.me>
2022-07-01 16:45:36 +0200

    related names fix

--- src/projects/models.py
+++ src/projects/models.py
@@ -26,9 +26,9 @@ class Issue(models.Model):
     tag = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='issues')
     author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
-    assignee = models.ForeignKey(to=get_user_model(), null=True, on_delete=models.SET_NULL, related_name='on_task')
+    assignee = models.ForeignKey(to=get_user_model(), null=True, on_delete=models.SET_NULL, related_name='assignees')
 
     def __str__(self):
         return self.title
@@ -38,7 +38,7 @@ class Comment(models.Model):
     content = models.TextField(max_length=2000)
     date_created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
-    issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
+    issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return self.author
