76d472d82a5e1121c078361d4105fc887952517a
Thomas Roussel <thomas.roussel@ik.me>
2022-07-01 15:41:11 +0200

    models admin list display

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 from .models import Project, Contributor, Issue, Comment
 
-admin.site.register(Project)
-admin.site.register(Contributor)
-admin.site.register(Issue)
-admin.site.register(Comment)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'type')
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'project', 'permission', 'role')
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'author', 'tag', 'status', 'date_created')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'issue', 'date_created')
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Comment, CommentAdmin)
