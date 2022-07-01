bf8a761905069377b6956fdec32a20ea10bd096b
Thomas Roussel <thomas.roussel@ik.me>
2022-07-01 11:57:19 +0200

    admin models registering

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Contributor, Issue, Comment
+
+admin.site.register(Project)
+admin.site.register(Contributor)
+admin.site.register(Issue)
+admin.site.register(Comment)
