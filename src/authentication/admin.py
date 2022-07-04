b385be97c0e6e5947d28e503fdcbe161beb87765
Thomas Roussel <thomas.roussel@ik.me>
2022-07-04 14:41:04 +0200

    list display user id

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id')
+
+
+admin.site.register(User, UserAdmin)
