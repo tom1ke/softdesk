7fb9c70ffd8a914c51e31b2583af82b6bb21d41b
Thomas Roussel <thomas.roussel@ik.me>
2022-07-07 17:17:43 +0200

    contributor object permission

--- src/projects/permissions.py
+++ src/projects/permissions.py
@@ -9,3 +9,11 @@ class IsAuthorAndOrIsAuthenticated(BasePermission):
     def has_object_permission(self, request, view, obj):
         return True if request.method in SAFE_METHODS else obj.author == request.user
 
+
+class IsProjectAuthorAndIsAuthenticated(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.project.author == request.user
