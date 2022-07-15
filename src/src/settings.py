776c4f709be26435ca40c2c7c6caa449abd0be17
Thomas Roussel <thomas.roussel@ik.me>
2022-07-15 14:19:44 +0200

    throttling default classes & rates

--- src/src/settings.py
+++ src/src/settings.py
@@ -144,4 +144,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '60/minute'
+    }
 }
